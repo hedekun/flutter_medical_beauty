4e364e2eafa6e23f4c1f85055d85239675c008fe
hedekun <hedekun0819@gmail.com>
2019-09-09 20:33:05 +0800

    tabbar添加

---
+++ lib/page/home/custom_sliver_tab.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_medical_beauty/common/style/colours.dart';
+import 'package:flutter_medical_beauty/component/sliver/SliverPersisteneHeaderDelegate.dart';
+
+class SliverPersistentTabBarPage extends StatefulWidget {
+  SliverPersistentTabBarPage({Key key, this.tabs, this.tabController}):super(key:key);
+  final List<Tab> tabs;
+  final TabController tabController;
+
+  @override
+  _SliverPersistentTabBarPage createState() => _SliverPersistentTabBarPage();
+}
+
+class _SliverPersistentTabBarPage extends State<SliverPersistentTabBarPage> with SingleTickerProviderStateMixin {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+      floating: true,
+      pinned: true,
+      delegate: CustomSliverPersistentHeaderDelegate(
+          max: 60,
+          min: 60,
+          child: TabBar(
+              tabs: widget.tabs,
+              controller: widget.tabController,
+              isScrollable: true,
+              indicatorColor: Colours.app_main,
+              labelColor: Colours.app_main,
+              labelStyle: TextStyle(fontSize: 16),
+              unselectedLabelColor: Colours.text_color,
+              unselectedLabelStyle: TextStyle(fontSize: 15),
+              indicatorSize: TabBarIndicatorSize.label)),
+    );
+  }
+}
--- lib/page/home/index.dart
+++ lib/page/home/index.dart
@@ -1,22 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_medical_beauty/common/style/colours.dart';
+import 'package:flutter_medical_beauty/component/sliver/SliverPersisteneHeaderDelegate.dart';
 import 'package:flutter_medical_beauty/page/home/navigation.dart';
 import 'package:flutter_medical_beauty/page/home/banner.dart';
 import 'package:flutter_medical_beauty/page/home/feature.dart';
 import 'package:flutter_medical_beauty/page/home/position.dart';
+import 'package:flutter_medical_beauty/page/home/custom_sliver_tab.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin {
+class _HomePageState extends State<HomePage>
+    with
+        AutomaticKeepAliveClientMixin<HomePage>,
+        SingleTickerProviderStateMixin {
+  TabController _tabController;
+  final List<Tab> tabs = [
+    Tab(
+      text: '推荐',
+    ),
+    Tab(
+      text: '眼部',
+    ),
+    Tab(
+      text: '鼻综合',
+    ),
+    Tab(
+      text: '玻尿酸',
+    ),
+    Tab(
+      text: '美体塑形',
+    ),
+    Tab(
+      text: '面部',
+    ),
+  ];
 
-  @protected
-  bool get wantKeepAlive=>true;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(vsync: this, length: tabs.length);
+  }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return SafeArea(
       child: CustomScrollView(
         physics: ScrollPhysics(),
@@ -30,11 +65,15 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
             color: Colours.workspace,
             width: double.infinity,
             height: 20,
-          ),)
+          ),),
+          SliverPersistentTabBarPage(tabs: tabs, tabController: _tabController,),
+          SliverToBoxAdapter(child: Container(
+            color: Colours.workspace,
+            width: double.infinity,
+            height: 2000,
+          ),),
         ],
       ),
     );
   }
-
-
 }
