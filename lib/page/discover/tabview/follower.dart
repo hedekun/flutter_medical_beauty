178a306c049643bc9ba5bd27d8a5b7ad8ed32d36
hedekun <hedekun0819@gmail.com>
2019-09-18 16:51:34 +0800

    发现页关注

--- lib/page/discover/tabview/follower.dart
+++ lib/page/discover/tabview/follower.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_medical_beauty/common/style/colours.dart';
 import 'package:flutter_medical_beauty/common/style/fontsize.dart';
-import 'package:flutter_medical_beauty/common/util/image_util.dart';
 import 'package:flutter_medical_beauty/page/discover/tabview/recommend.dart';
 import 'package:flutter_medical_beauty/page/home/tabview/recommend.dart';
 
@@ -49,54 +48,6 @@ class FollowerForYouTitle extends StatelessWidget {
                 ],
               )
             ],
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _doctorCard(context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 5),
-      width: (MediaQuery.of(context).size.width - 32 - 20) / 3,
-      height: 170,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          border: Border.all(color: Colours.navigator_filled)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          loadAssetImage('head_image/5', width: 50, fit: BoxFit.fitWidth),
-          Text('赵志明'),
-          Text(
-            '高级医师',
-            style: TextStyle(
-                color: Colours.recommend_title, fontSize: FontSize.small_size),
-          ),
-          SizedBox(height: 10),
-          Container(
-            width: 66,
-            height: 26,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(2)),
-              border: Border.all(color: Colours.app_main, width: 1),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Icon(
-                  Icons.add,
-                  color: Colours.app_main,
-                ),
-                Text(
-                  '关注',
-                  style: TextStyle(
-                      color: Colours.app_main, fontSize: FontSize.small_size),
-                )
-              ],
-            ),
           )
         ],
       ),
